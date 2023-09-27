8159d16f6d18eb8398682b375938bb003f95800e
Iqbal Fauzi <iqbalfawz@gmail.com>
2023-09-27 11:57:27 +0700

    Fix broken encoding

--- modules/cli-to/config.php
+++ modules/cli-to/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'cli-to',
-    '__version' => '0.1.1',
+    '__version' => '0.2.0',
     '__git' => 'git@github.com:getmim/cli-to.git',
     '__license' => 'MIT',
     '__author' => [
